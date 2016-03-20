abc0c98009ebb42693a45ef4b7c6396786c9b149
Leonardo Gatica <lgatica@protonmail.com>
2016-03-20 17:17:04 -0300

    Remove run player. Fix output

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 
 'use strict';
 
-import {spawn} from 'child_process';
 import program from 'commander';
 import animeDl from 'anime-dl';
 import chalk from 'chalk';
@@ -17,18 +16,15 @@ program
   .description('CLI for get chapter link')
   .option('-a, --anime [anime]', 'Add name')
   .option('-c, --chapter [chapter]', 'Add chapter')
-  .option('-p, --player', 'Run player (optional). Availables: mpv|vlc|mplayer|omxplayer|smplayer|cvlc. Default: mpv', /(mpv|vlc|mplayer|omxplayer|smplayer|cvlc)/i, 'mpv')
   .parse(process.argv);
 
 if (program.anime && program.chapter) {
+  console.log(chalk.green('Searching...'));
   animeDl.getLinksByNameAndChapter(program.anime, program.chapter).then((data) => {
     if (data.urls.length === 0) return console.log(chalk.red('Not found a link'));
-    const chapter = data.urls[data.urls.length - 1];
-    if (program.player) {
-      console.log(chalk.green(`Wait running ${program.player} ${chapter} ...`));
-      spawn(program.player, [chapter], {detached: true, stdio: 'ignore'});
-    } else {
-      console.log(chalk.green(`Run ${chapter} in any player`));
+    console.log(chalk.green('Run any this links in your video player'));
+    for (let url of data.urls) {
+      console.log(chalk.green(url));
     }
   }).catch((err) => console.log(chalk.red(`Error: ${err.message}`)));
 } else {
