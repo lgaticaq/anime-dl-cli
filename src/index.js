301ae3dc8ba7425830b48835768d2cf2623af948
Leonardo Gatica <lgatica@protonmail.com>
2016-08-15 00:51:35 -0300

    feat(player): Add suport play chapter in mpv

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const chalk = require('chalk');
 const updateNotifier = require('update-notifier');
 const pkg = require('../package.json');
 const ora = require('ora');
+const spawn = require('child_process').spawn;
 
 updateNotifier({pkg}).notify();
 
@@ -17,6 +18,7 @@ program
   .description('CLI to get a chapter link')
   .option('-a, --anime [anime]', 'Add name')
   .option('-c, --chapter [chapter]', 'Add chapter')
+  .option('-k, --mpv', 'Play chapter in mpv')
   .parse(process.argv);
 
 if (program.anime && program.chapter) {
@@ -25,9 +27,15 @@ if (program.anime && program.chapter) {
   animeDl.getLinksByNameAndChapter(program.anime, program.chapter).then((data) => {
     spinner.stop();
     if (data.urls.length === 0) return console.log(chalk.red('No links found'));
-    console.log(chalk.green('Run any of these links in your video player'));
-    for (let url of data.urls) {
-      console.log(chalk.green(url));
+    if (program.mpv) {
+      const url = data.urls[0];
+      console.log(chalk.green(`Playing ${url} in mpv`));
+      spawn('mpv', [url], {detached: true, stdio: 'ignore'});
+    } else {
+      console.log(chalk.green('Run any of these links in your video player'));
+      for (let url of data.urls) {
+        console.log(chalk.green(url));
+      }
     }
   }).catch((err) => {
     spinner.stop();
