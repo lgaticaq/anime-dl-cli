024ee679377e490a3d8ac912b47d8271cb54bcbf
Leonardo Gatica <lgatica@protonmail.com>
2016-10-15 19:40:50 -0300

    feat(download): Add download chapter option

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const program = require('commander');
 const animeDl = require('anime-dl');
 const chalk = require('chalk');
@@ -19,18 +21,47 @@ program
   .option('-a, --anime [anime]', 'Add name')
   .option('-c, --chapter [chapter]', 'Add chapter')
   .option('-k, --mpv', 'Play chapter in mpv')
+  .option('-d, --download [directory]', 'Download chapter to directory')
   .parse(process.argv);
 
 if (program.anime && program.chapter) {
-  const spinner = ora('Searching...');
+  const spinner = ora('Searching');
   spinner.start();
   animeDl.getLinksByNameAndChapter(program.anime, program.chapter).then((data) => {
-    spinner.stop();
-    if (data.urls.length === 0) return console.log(chalk.red('No links found'));
+    if (data.urls.length === 0) {
+      spinner.text = 'No links found';
+      spinner.fail();
+      return;
+    }
+    spinner.succeed();
     if (program.mpv) {
       const url = data.urls[0];
       console.log(chalk.green(`Playing ${url} in mpv`));
       spawn('mpv', [url], {detached: true, stdio: 'ignore'});
+    } else if (typeof program.download !== 'undefined') {
+      if (program.download === true) {
+        program.download = process.cwd();
+      }
+      if (!fs.existsSync(program.download)) {
+        spinner.text = 'Destination no exist';
+        spinner.fail();
+        return;
+      }
+      const filename = path.join(program.download, `${data.title} ${data.chapter}.mp4`);
+      if (fs.existsSync(filename)) {
+        spinner.text = `Destination ${filename} already exist`;
+        spinner.fail();
+        return;
+      }
+      const url = data.urls[0];
+      spinner.text = `Downloading ${data.title} chapter ${data.chapter}`;
+      spinner.start();
+      const wget = spawn('wget', [url, '-O', filename], {detached: true, stdio: 'ignore'});
+      wget.on('close', () => {
+        spinner.test =  'Finish';
+        spinner.succeed();
+        return;
+      });
     } else {
       console.log(chalk.green('Run any of these links in your video player'));
       for (let url of data.urls) {
@@ -38,8 +69,8 @@ if (program.anime && program.chapter) {
       }
     }
   }).catch((err) => {
-    spinner.stop();
-    console.log(chalk.red(`Error: ${err.message}`));
+    spinner.text = `Error: ${err.message}`;
+    spinner.fail();
   });
 } else {
   program.help();
